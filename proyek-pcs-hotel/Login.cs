17b5d6ad932615ee058b34a179fdf7175101ae7f
Gerry Surya <ger_26_6@hotmail.com>
2017-05-01 15:54:11 +0700

    Login Form

---
+++ proyek-pcs-hotel/Login.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace proyek_pcs_hotel
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+        public string mode;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (mode == "Receptionist")
+            {
+
+            }
+            else if (mode == "Restoran")
+            {
+
+            }
+            else if (mode == "Dapur")
+            {
+
+            }
+            else if (mode == "Laundry")
+            {
+
+            }
+            else if (mode =="Self Booking")
+            {
+
+            }
+            else if (mode == "HRD")
+            {
+
+            }
+            else if (mode == "Laporan")
+            {
+
+            }
+        }
+    }
+}
