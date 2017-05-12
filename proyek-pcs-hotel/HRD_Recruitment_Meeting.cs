bcf1f3ea589ad7d1932f22787fd715e5447ee056
Gerry Surya <ger_26_6@hotmail.com>
2017-05-12 13:30:41 +0700

    HRD Forms

---
+++ proyek-pcs-hotel/HRD_Recruitment_Meeting.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace proyek_pcs_hotel
+{
+    public partial class HRD_Recruitment_Meeting : UserControl
+    {
+        public HRD_Recruitment_Meeting()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- proyek-pcs-hotel/MainMenu.Designer.cs
+++ proyek-pcs-hotel/MainMenu.Designer.cs
@@ -35,14 +35,14 @@
             this.button5 = new System.Windows.Forms.Button();
             this.button6 = new System.Windows.Forms.Button();
             this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(18, 18);
-            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Location = new System.Drawing.Point(12, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(390, 34);
+            this.button1.Size = new System.Drawing.Size(260, 23);
             this.button1.TabIndex = 0;
             this.button1.Text = "Receptionist";
             this.button1.UseVisualStyleBackColor = true;
@@ -50,10 +50,9 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(18, 62);
-            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Location = new System.Drawing.Point(12, 41);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(390, 34);
+            this.button2.Size = new System.Drawing.Size(260, 23);
             this.button2.TabIndex = 1;
             this.button2.Text = "Restoran";
             this.button2.UseVisualStyleBackColor = true;
@@ -61,10 +60,9 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(18, 105);
-            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Location = new System.Drawing.Point(12, 70);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(390, 34);
+            this.button3.Size = new System.Drawing.Size(260, 23);
             this.button3.TabIndex = 2;
             this.button3.Text = "Dapur";
             this.button3.UseVisualStyleBackColor = true;
@@ -72,10 +70,9 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(18, 148);
-            this.button4.Margin = new System.Windows.Forms.Padding(4);
+            this.button4.Location = new System.Drawing.Point(12, 99);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(390, 34);
+            this.button4.Size = new System.Drawing.Size(260, 23);
             this.button4.TabIndex = 3;
             this.button4.Text = "Laundry";
             this.button4.UseVisualStyleBackColor = true;
@@ -83,10 +80,9 @@
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(18, 192);
-            this.button5.Margin = new System.Windows.Forms.Padding(4);
+            this.button5.Location = new System.Drawing.Point(12, 128);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(390, 34);
+            this.button5.Size = new System.Drawing.Size(260, 23);
             this.button5.TabIndex = 4;
             this.button5.Text = "Self Booking";
             this.button5.UseVisualStyleBackColor = true;
@@ -94,10 +90,9 @@
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(18, 236);
-            this.button6.Margin = new System.Windows.Forms.Padding(4);
+            this.button6.Location = new System.Drawing.Point(12, 157);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(390, 34);
+            this.button6.Size = new System.Drawing.Size(260, 23);
             this.button6.TabIndex = 5;
             this.button6.Text = "HRD";
             this.button6.UseVisualStyleBackColor = true;
@@ -105,20 +100,29 @@
             // 
             // button7
             // 
-            this.button7.Location = new System.Drawing.Point(18, 279);
-            this.button7.Margin = new System.Windows.Forms.Padding(4);
+            this.button7.Location = new System.Drawing.Point(12, 186);
             this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(390, 34);
+            this.button7.Size = new System.Drawing.Size(260, 23);
             this.button7.TabIndex = 6;
             this.button7.Text = "Laporan";
             this.button7.UseVisualStyleBackColor = true;
             this.button7.Click += new System.EventHandler(this.button7_Click);
             // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(12, 216);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(260, 23);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "Lamar Pekerjaan";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
             // MainMenu
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(144F, 144F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
-            this.ClientSize = new System.Drawing.Size(426, 338);
+            this.ClientSize = new System.Drawing.Size(284, 250);
+            this.Controls.Add(this.button8);
             this.Controls.Add(this.button7);
             this.Controls.Add(this.button6);
             this.Controls.Add(this.button5);
@@ -127,7 +131,6 @@
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "MainMenu";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Main Menu";
@@ -144,6 +147,7 @@
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Button button6;
         private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
     }
 }
 
